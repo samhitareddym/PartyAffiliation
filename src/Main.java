acb3eb9a625de2fc35dad2cd663ff39069000a21
samhitareddy <samhitareddy0402@gmail.com>
2024-09-30 16:05:32 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Create a Scanner object to read input
+        Scanner in = new Scanner(System.in);
+    }
+}
